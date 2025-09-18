52341fdb844cd4bb431a58eedaa46d47e5a8cf2b
AceyMich <joshuasy565@gmail.com>
2025-09-18 20:25:55 +0800

    Add files via upload

---
+++ EmployementManagement/Model/EmployeeDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.Models
+{
+    public class EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employeelist { get; set; }
+    }
+}
+
+
---
+++ EmployementManagement/Model/EmployeeInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public required int ListId { get; set; }
+        public  required string? Id { get; set; }
+      
+        public  string? Name { get; set; }
+        public  string? Position { get; set; }
+        public  string? Department { get; set; }
+        public  string? Status { get; set; }
+        
+    }
+
+
+
+    
+}
+
